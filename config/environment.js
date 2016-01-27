167521f9fd8aec28525e93a6e2034e36fa77d468
stevenpslade <stevenpslade@gmail.com>
2016-01-26 23:57:18 -0800

    set up ember and firebase

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ember2-blog',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://ember-blog-slade.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
